ff1dca738e270de5e9baa69393cb5dbf8a3beba3
Blade Heda <nkwetakembrunodudu9@gmail.com>
2023-03-11 23:00:42 +0100

    just created form

---
+++ form.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <div class="div-name">
+            <label for="name">Name</label>
+            <input type="text" placeholder="Enter your name" class="name">
+        </div>
+        <div class="div-email">
+            <label for="email">Email</label>
+            <input type="email" placeholder="Enter your Email" class="name">
+        </div>
+        <div class="div-feedback">
+            <label for="feedback">Feedback</label>
+            <input type="text" placeholder="Enter yur Feedback" class="name">
+        </div>
+    
+    </form>
+    
+</body>
+</html>
