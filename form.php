dd8297791533bca76c0f2fc6d78481cafddb97d0
Blade Heda <nkwetakembrunodudu9@gmail.com>
2023-03-11 23:39:13 +0100

    just created form

--- form.php
+++ form.php
@@ -9,26 +9,45 @@
 </head>
 
 <body>
-    <form action="">
+    <!-- write the logic to validate the form  -->
+    <?php
+    // define the initials for the variables of the form 
+    $name= $email = $Feedback = " " ; 
+    // create a function to text the nature of the input 
+    function testData($data){
+        // ?check if the word had space before and after , backslashes and special chaharacters
+        $data = trim($data);
+        $data = stripslashes($data); 
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+  if ($_SERVER["REQUEST METHOD"]=="POST"){
+    $name= testData($_POST["name"]);
+    $email = testData(($_POST["email"]));
+    $Feedback = testData($_POST("feedback"));
+  }
+
+    ?>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP SELF"]); ?>    ">
         <div class="div-name">
             <div>
             <label for="name">Name</label>
             </div>
-            <input type="text" placeholder="Enter your name" class="name">
+            <input type="text" name="name" placeholder="Enter your name" class="name">
         </div>
         <div class="div-email">
         <div>
             <label for="email">Email</label>
         </div>
-            <input type="email" placeholder="Enter your Email" class="name">
+            <input type="email" name="email" placeholder="Enter your Email" class="name">
         </div>
         <div class="div-feedback">
         <div>
             <label for="feedback">Feedback</label>
           </div>
-            <input type="text" placeholder="Enter yur Feedback" class="name">
+            <input type="text name  feedback" placeholder="Enter yur Feedback" class="name">
         </div>
-
+    <button>Send </button>
     </form>
 
 </body>
