28ff278186a9d3783319ccb3625b2003209f4bf2
Blade Heda <nkwetakembrunodudu9@gmail.com>
2023-03-16 21:44:46 +0100

    buggy code

--- Objects.php
+++ Objects.php
@@ -24,3 +24,24 @@ $alex->say_name();
 // this will not work
 // echo $alex->full_name();
 ?>
+<?php
+// TODO: Implement the Car class here
+class Car {
+    public function _construct($brand, $make_year) 
+    {
+    $this->brand=$brand;
+    $this->make_year=$make_year;
+    }
+
+// public function print_details() {
+public function print_details() {
+echo "brand name is ".$this->brand." made in".$this->make_year;
+}
+}
+
+$car = new Car("Toyota", 2006);
+$car->print_details();
+?>
+
+
+
